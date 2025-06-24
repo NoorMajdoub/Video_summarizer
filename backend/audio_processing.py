21da012bd3696c2e8cd799b680552fd5c3812873
NoorMajdoub <nourelhoudamajdoub1@gmail.com>
2025-06-24 23:16:31 +0100

    foudn the transcription logic and added it

---
+++ backend/audio_processing.py
@@ -0,0 +1,15 @@
+
+from youtube_transcript_api import YouTubeTranscriptApi
+
+
+# get id of the youtube video 
+def get_video_id(url):
+    return url.split("watch?v=")[-1]
+
+def get_transcript(url_vid):
+    id_vid=get_video_id(url_vid)
+    transcript = YouTubeTranscriptApi.get_transcript(id_vid)
+   # frames=get_frames(transcript)
+    tran_joined=" ".join((line["text"] for line in transcript))
+    return tran_joined
+print(get_transcript("https://www.youtube.com/watch?v=o06MyVhYte4"))
