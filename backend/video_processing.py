f623faa2d1d41d5be77327603cf5cfe17c004343
Nour Majdoub <154745715+NoorMajdoub@users.noreply.github.com>
2026-04-29 15:00:43 +0100

    Enhance video processing with new features and comments
    Added functions for video processing, including histogram calculation, timestamp extraction, and code detection from frames. Improved structure and added comments for clarity.

--- backend/video_processing.py
+++ backend/video_processing.py
@@ -1,4 +1,7 @@
-
+"""
+Video_processing.py
+Main code for extracting the code from the video
+"""
 import yt_dlp
 import cv2
 from PIL import Image
@@ -7,20 +10,28 @@ import numpy as np
 from transformers import CLIPProcessor, CLIPModel
 from PIL import Image
 import torch
+import os
+import easyocr
+import cv2
+from difflib import SequenceMatcher
+
 def code_extraction_pipeline(name,vid_url):
+    model,processor=get_model()
     print("----------------Downloading the video------------")
     download_vid(name, vid_url)  #the url the user puts
     print("----------------Getting time stanmps------------")
 
-    time_stamps=get_timestamps(video_path) #the video path is after it is downladed
+    time_stamps=get_timestamps("/kaggle/working/downloaded_videodes.mp4") #the video path is after it is downladed
     print("----------------Getting code intervals------------")
 
-    code_intervals=detect_code_intervals(time_stamps)
+    code_intervals=detect_code_intervals(time_stamps,model,processor)
     print("----------------getting the code with ocr------------")
 
     code=get_code_with_ocr(code_intervals)
-    print("Cleaning code with LLM")
-    return code
+    print("--------------Cleaning code with LLM-----------------")
+    clean_code=call_llm(get_code_cleaning_prompt(code))
+    return clean_code
+    
 def download_vid(name, url):
     ydl_opts = {
         'outtmpl': f'downloaded_video{name[:3]}.%(ext)s',
@@ -30,7 +41,6 @@ def download_vid(name, url):
         'ignoreerrors': True,
         'retries': 10,
         'fragment_retries': 10,
-        # Avoid AV1 formats
         'format_sort': ['+codec:h264,+codec:vp9'],
         'prefer_free_formats': False,
         'extractor_args': {
@@ -44,6 +54,7 @@ def download_vid(name, url):
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
+        
 def get_histogram(image):
     """
     Gets image and returns histogram
@@ -52,12 +63,21 @@ def get_histogram(image):
     hist = cv2.calcHist([hsv], [0, 1], None, [50, 60], [0, 180, 0, 256])
     cv2.normalize(hist, hist)
     return hist
+    
 def format_time(sec):
     minutes = sec // 60
     seconds = sec % 60
     return f"{minutes}:{seconds:02d}"
-def get_timestamps(video_path):
     
+def get_timestamps(video_path):
+            """
+            Function to get the timestamps of the video , by sampling the video (every 3 seconds) and comparing the changes in
+            histogram to detect changes in the frame
+            Args:
+                Input : video path (after it is downloaded , it is fixed because the backend is running on kaggle)
+            Returns:
+                Array of timestamps
+            """
             
             capture = cv2.VideoCapture(video_path)
             timestamps=[]
@@ -84,7 +104,7 @@ def get_timestamps(video_path):
                 success, frame = capture.read()
                 diff = cv2.compareHist(get_histogram(last_frame), get_histogram(frame), cv2.HISTCMP_BHATTACHARYYA)
             
-                if diff > 0.6:
+                if diff > 0.6:  #threshold to detect the changes in frames
                     timestamps.append((format_time(last_save), format_time(sec)))
                     print(format_time(sec))
                     cv2.imwrite(f"d{sec}.png", frame)  # boundary frame
@@ -99,38 +119,26 @@ def get_timestamps(video_path):
             
             capture.release()
             return timestamps
+    
 def get_model():
+    """
+    The clip model used to detect code
+    """
     model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
     processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
     return model, processor
 
 
-def classify_frame(image_path):
-    image = Image.open(image_path).convert("RGB")
-    w, h = image.size
-    image = image.crop((0, 0, int(w * 0.85), int(h * 0.85)))
-    
-    text_prompts = [
-        "programming code in a text editor",
-        "a slide presentation or a web browser,",
-            "a title slide with large text or heading",
 
-        "a person or abstract background"
-    ]
-    inputs = processor(text=text_prompts, images=image, return_tensors="pt", padding=True)
-    with torch.no_grad():
-        outputs = model(**inputs)
-    
-    # look at RAW similarities before softmax
-    raw = (outputs.image_embeds @ outputs.text_embeds.T).squeeze()
-    print("raw scores:", raw)
-    
-    # also try with temperature scaling like CLIP actually does internally
-    logits = outputs.logits_per_image  # this is the RIGHT way to get scores from CLIP
-    print("logits:", logits)
-    probs = logits.softmax(dim=-1)
-    print("probs from logits:", probs)
-def isCodeFrame(image_path):
+def isCodeFrame(image_path,model,processor):
+    """
+    Function to return True/False of presence of code in a frame
+    Uses 3 Prompts(the texts) embeddings to get the logits of matching #TODO improve description
+    Args:
+        Input: image , model , processor
+    Returns :
+        Probs of matching of the image with each of the three text promtps
+    """
     image = Image.open(image_path).convert("RGB")
     w, h = image.size
     image = image.crop((0, 0, int(w * 0.85), int(h * 0.85)))
@@ -147,12 +155,14 @@ def isCodeFrame(image_path):
     probs = outputs.logits_per_image.softmax(dim=-1).squeeze()
     
     print(f"code:{probs[0]:.3f} slide:{probs[1]:.3f} other:{probs[2]:.3f}  ---{image_path}")
-    return probs[0].item() > 0.9
-"""# test all your frames
-for path in code_frames + no_code_frames :
-    print(f"\n--- {path.split('/')[-1]} ---")
-    print(isCodeFrame(path))"""
-def detect_code_intervals(timestamps, interval=3):
+    return probs[0].item() > 0.9  #higher threshold to be sure 
+
+def detect_code_intervals(timestamps,model,processor, interval=3):
+    """
+    Function that will run the code detection approach on the timestamps to detect which are of code
+    Runs is_code function on the beginning of the interval , the end ,and two random frames from the middle
+    If 2/4 of the frames are of code, then the interval is detected to contain code 
+    """
     code_intervals = []
     
     for begin, end in timestamps:
@@ -172,10 +182,10 @@ def detect_code_intervals(timestamps, interval=3):
         mid1_path  = f"/kaggle/working/all{mid1_sec}.png"
         mid2_path  = f"/kaggle/working/all{mid2_sec}.png"
         
-        res_beg  = isCodeFrame(begin_path)
-        res_mid1 = isCodeFrame(mid1_path)
-        res_mid2 = isCodeFrame(mid2_path)
-        res_end  = isCodeFrame(end_path)
+        res_beg  = isCodeFrame(begin_path,model,processor)
+        res_mid1 = isCodeFrame(mid1_path,model,processor)
+        res_mid2 = isCodeFrame(mid2_path,model,processor)
+        res_end  = isCodeFrame(end_path,model,processor)
         
         votes = [res_beg, res_mid1, res_mid2, res_end]
         print(f"{begin}-{end} | {votes} | sum={sum(votes)}")
@@ -188,18 +198,39 @@ def detect_code_intervals(timestamps, interval=3):
 def time_to_sec(t):
     parts = t.split(":")
     return int(parts[0]) * 60 + int(parts[1])
-from difflib import SequenceMatcher
 
 def is_duplicate(new_text, seen_texts, threshold=0.85):
     for seen in seen_texts:
         if SequenceMatcher(None, new_text, seen).ratio() > threshold:
             return True
     return False
-def get_code_with_ocr(p):
+def easy_ocr(image,reader):
+      """
+      Helper funcction to apply the ocr on the image
+      """
+      data=""
+      image = cv2.imread(image)
+      try:
+        result = reader.readtext(image)
+        for _, text, _ in result:
+             data+=text
+        return data
+      except:
+       return data
+def get_code_with_ocr(code_intervals):
+        """
+        Gets the code from the code intervals 
+        Args:
+            The code intervals timestamps
+        Returns :
+            all the code extracted in one array
+        
+        """
         all_code = []
         seen_texts = []
-        
-        for begin, end in p:
+        reader = easyocr.Reader(['en'])
+
+        for begin, end in code_intervals:
             begin_sec = time_to_sec(begin)
             end_sec = time_to_sec(end)
             
@@ -213,7 +244,7 @@ def get_code_with_ocr(p):
                 if not os.path.exists(path):
                     continue
                 
-                text = easy_ocr(path)
+                text = easy_ocr(path,reader)
                 
                 if not text.strip():
                     continue
